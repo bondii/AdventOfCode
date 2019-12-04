7a7f796e2b2c9544495a3fa6b89b1136dfa3e0ad
Pontus Eriksson <pontus.eriksson@omegapoint.se>
2019-12-04 16:56:37 +0100

    WIP

--- 2019/day3/solution1.py
+++ 2019/day3/solution1.py
@@ -3,7 +3,7 @@ file = open('input')
 path1 = [(x[0], int(x[1:])) for x in file.readline().split(',')]
 path2 = [(x[0], int(x[1:])) for x in file.readline().split(',')]
 
-hasPassed = [set((0, 0)), set((0, 0))]
+hasPassed = (set(), set())
 
 
 def traversePath(walker):
@@ -31,3 +31,15 @@ def traversePath(walker):
             exit()
 
 
+traversePath(0)
+traversePath(1)
+minDist = 1e9
+for location in hasPassed[0]:
+    if location in hasPassed[1]:
+        minDist = min(minDist, (location[0] + location[1]))
+
+if minDist != 1e9:
+    print('The manhattan distance from the central port to the closest ',
+          'intersection is:', minDist)
+else:
+    print('No intersection found')
